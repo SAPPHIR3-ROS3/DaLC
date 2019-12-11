6003e7c6f13870f16f492198750b5ee0b45ab673
Patrizio Onida <patriziosimone@hotmail.it>
2019-12-11 19:17:01 +0100

    Create DaLC.py

---
+++ DaLC.py
@@ -0,0 +1,22 @@
+from PIL import Image
+from ctypes import windll as WDLL
+from os.path import abspath as Path
+from PIL.ImageFilter import GaussianBlur as Blur
+
+def Main():
+    print("Drag n Drop the image file here and press Enter", end = ' ')
+    File = input()
+    Desktop = Image.open(repr(File)[1:len(repr(File)) - 1])
+    Desktop.save('WallPaper.png')
+    Desktop.save('Login.png')
+    Login = Image.open('Login.png')
+    Login = Login.filter(Blur(Percent(3, list(Login.size)))).save('Login.png')
+    WDLL.user32.SystemParametersInfoW(0x14, 0, Path('WallPaper.png'), 3)
+
+def Percent(Percentage = float(), Dimensions = []):
+    print(Dimensions[0])
+    Size = min(Dimensions)
+    Pixels = int((Percentage * Size) / 100)
+    return Pixels
+
+Main()
