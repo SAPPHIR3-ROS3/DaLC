dcd4b16851da15c4187867fd8a99121d4674fc89
Patrizio <patriziosimone@hotmail.it>
2020-03-20 13:45:03 +0100

    added print and input statement

--- DaLC.py
+++ DaLC.py
@@ -12,6 +12,8 @@ def Main():
     Login = Image.open('Login.png')
     Login = Login.filter(Blur(Percent(3, list(Login.size)))).save('Login.png')
     WDLL.user32.SystemParametersInfoW(0x14, 0, Path('WallPaper.png'), 3)
+    print("Desktop changed suu=ccessfully")
+    input()
     pass
 
 def Percent(Percentage = float(), Dimensions = []):
